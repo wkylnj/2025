e3cab4969efd83cbf5165c190e061f2bb107d429
diaohongning <etodemerzel@qq.com>
2025-11-10 23:00:03 +0800

    add fancybutton function

--- Sources/Fancybutton/Fancybutton.swift
+++ Sources/Fancybutton/Fancybutton.swift
@@ -1,2 +1,30 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+public struct FancyButton: View {
+    private var title: String
+    private var action: () -> Void
+    public init(_ title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+    public var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.headline)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(
+                    LinearGradient(
+                        colors: [.purple, .blue],
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    )
+                )
+                .foregroundColor(.white)
+                .cornerRadius(12)
+                .shadow(radius: 5)
+        }
+        .buttonStyle(.plain)
+    }
+}
+
